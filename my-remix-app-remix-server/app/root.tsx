0c15e744c5a16adc7a986832a20f0d1bef973011
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2021-12-05 10:31:57 +0900

    tutorial:errorの途中まで

--- my-remix-app-remix-server/app/root.tsx
+++ my-remix-app-remix-server/app/root.tsx
@@ -1,5 +1,5 @@
 import type { LinksFunction } from "remix";
-import { Links, LiveReload, Outlet } from "remix";
+import { Links, LiveReload, Outlet, useCatch, Scripts } from "remix";
 
 import globalStylesUrl from "./styles/global.css";
 import globalMediumStylesUrl from "./styles/global-medium.css";
@@ -24,21 +24,64 @@ export let links: LinksFunction = () => {
   ];
 };
 
-export default function App() {
+function Document({
+  children,
+  title = `Remix: So great, it's funny!`
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <title>Remix: So great, it's funny!</title>
-        {/* CSSのスタイリング読み込み */}
+        <title>{title}</title>
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
+        <Scripts />
         {process.env.NODE_ENV === "development" ? (
           <LiveReload />
         ) : null}
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <Document
+      title={`${caught.status} ${caught.statusText}`}
+    >
+      <div className="error-container">
+        <h1>
+          {caught.status} {caught.statusText}
+        </h1>
+      </div>
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
+  return (
+    <Document title="Uh-oh!">
+      <div className="error-container">
+        <h1>App Error</h1>
+        <pre>{error.message}</pre>
+      </div>
+    </Document>
   );
 }
